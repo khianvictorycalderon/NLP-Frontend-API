cff6ee146af070cac9fef7f359b11f30f8163632
khianvictorycalderon <ckhianvictory@gmail.com>
2026-02-24 20:24:55 +0800

    Test

--- nlp.js
+++ nlp.js
@@ -1,5 +1,10 @@
 window.API = {
     generateResponse: (input) => {
+        if (!Array.isArray(input)) {
+            console.error("getResponse input is not an array!");
+            return "Could not resolve conversation!";
+        }
 
+        return "Yey!";
     }
 }
